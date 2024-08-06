a67620f7b0f81f4b4a688ffa2d2494892bf58ba0
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2024-08-06 00:26:02 -0300

    gitignore

--- aqua_conecta/lib/main.dart
+++ aqua_conecta/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'routes.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:aqua_conecta/services/firebase_options.dart';
+import 'package:aqua_conecta/firebase_options.dart';
 import 'package:aqua_conecta/view_models/cadastro_view_model.dart'; // Adicione esta linha
 import 'package:aqua_conecta/view_models/login_view_model.dart'; // Adicione esta linha
 
