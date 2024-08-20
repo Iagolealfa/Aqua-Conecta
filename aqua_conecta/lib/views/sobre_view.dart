44d32d26bbb9165e32cc25a20a1c52034f76dbff
Guilherme de Moraes <gmsgui1000@gmail.com>
2024-08-20 18:51:29 -0300

    :sparkles: Novo: Sobre e Suporte
    Telas agora com conteúdo previsto pelo Figma

--- aqua_conecta/lib/views/sobre_view.dart
+++ aqua_conecta/lib/views/sobre_view.dart
@@ -14,15 +14,38 @@ class _SobreViewState extends State<SobreView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const AppDrawer(), // Adiciona o Drawer
+      drawer: const AppDrawer(),
       body: Stack(
         children: [
           Container(
-            padding: const EdgeInsets.only(top: 60, left: 40, right: 40),
-            color: Colors.white,
-            child: ListView(
-              children: const <Widget>[
-                SizedBox(height: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            color: Color.fromRGBO(114, 154, 214, 1), // Cor de fundo
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 60),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'O objetivo do nosso aplicativo é abordar o problema da escassez de água, buscando soluções colaborativas para apoiar aqueles que enfrentam desafios relacionados à falta d\'água. Estamos comprometidos em criar uma plataforma que una as pessoas e ofereça recursos para lidar com essa questão crucial.',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'version   1.0 - magikarp',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ],
             ),
           ),
--- aqua_conecta/lib/views/suporte_view.dart
+++ aqua_conecta/lib/views/suporte_view.dart
@@ -14,15 +14,164 @@ class _SuporteViewState extends State<SuporteView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const AppDrawer(), // Adiciona o Drawer
+      drawer: const AppDrawer(),
       body: Stack(
         children: [
           Container(
-            padding: const EdgeInsets.only(top: 60, left: 40, right: 40),
+            padding: const EdgeInsets.only(top: 35, left: 25, right: 25),
             color: Colors.white,
-            child: ListView(
-              children: const <Widget>[
-                SizedBox(height: 20),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      const Text(
+                        'PRINCIPAIS PERGUNTAS',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 20),
+                      ExpansionTile(
+                        title: const Text('Como reportar vazamento/qualidade da água?'),
+                        children: const <Widget>[
+                          ListTile(
+                            title: Text(
+                              '1- Na tela principal (home) clique no ícone "+"',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              '2- Ao aparecer o popup você pode escolher entre reportar vazamento de água ou a qualidade da mesma',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ExpansionTile(
+                        title: const Text('O que significam os ícones no mapa?'),
+                        children: const <Widget>[
+                          ListTile(
+                            title: Text(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ExpansionTile(
+                        title: const Text('Como acessar seu perfil?'),
+                        children: const <Widget>[
+                          ListTile(
+                            title: Text(
+                              '1- No menu lateral (drawer) clique na imagem de perfil que aparece no topo',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              '2- Depois de clicado você será levado para a aba de perfil',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ExpansionTile(
+                        title: const Text('Como verificar os relatórios gerados?'),
+                        children: const <Widget>[
+                          ListTile(
+                            title: Text(
+                              '1- No menu lateral (drawer) clique em “Relatórios”',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              '2- Depois de clicado você será levado para a aba de relatórios',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              '3- Dentro você pode selecionar o tipo de relatório que gostaria de visualizar',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ExpansionTile(
+                        title: const Text('Como reportar disponibilidade da água?'),
+                        children: const <Widget>[
+                          ListTile(
+                            title: Text(
+                              '1- Na tela inicial clique no  botão flutuante com o símbolo de água, localizado no canto inferior direito.',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(91, 91, 91, 1),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Ainda precisa de ajuda?',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Elaborar tela
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                          backgroundColor: Colors.blue,
+                        ),
+                        child: const Text(
+                          'Envie uma mensagem',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 15)
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
