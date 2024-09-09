921d1df144801161758504e3143130cc7bc5dfa4
Caio Mizan <caiomizan1@gmail.com>
2024-09-08 23:07:38 -0300

    implementada logica para adicionar foto

--- aqua_conecta/lib/views/report_qualidade.dart
+++ aqua_conecta/lib/views/report_qualidade.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart'; // Import necessário
+import 'dart:io'; // Import para trabalhar com arquivos
 
 class ReportQualidade extends StatefulWidget {
   @override
-  _ReportQualidade createState() => _ReportQualidade();
+  _ReportQualidadeState createState() => _ReportQualidadeState();
 }
 
-class _ReportQualidade extends State<ReportQualidade> {
+class _ReportQualidadeState extends State<ReportQualidade> {
   // Variáveis para manter o estado dos checkboxes
   bool isTurvaSelected = false;
   bool isMauCheiroSelected = false;
   bool isOutrosSelected = false;
+  File? _image; // Variável para armazenar a imagem capturada
+  bool _isPickingImage = false; // Variável de controle
+
+  // Método para capturar a imagem com a câmera
+  Future<void> _pickImage() async {
+    if (_isPickingImage) return; // Não faz nada se já está ativo
+
+    setState(() {
+      _isPickingImage = true;
+    });
+
+    try {
+      final ImagePicker picker = ImagePicker();
+      final XFile? pickedImage =
+          await picker.pickImage(source: ImageSource.camera);
+
+      if (pickedImage != null) {
+        setState(() {
+          _image = File(pickedImage.path);
+        });
+      }
+    } catch (e) {
+      // Tratamento de exceções, se necessário
+      print(e);
+    } finally {
+      setState(() {
+        _isPickingImage = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,18 +65,22 @@ class _ReportQualidade extends State<ReportQualidade> {
               'Adicione uma foto e selecione o(s) problema(s)',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
-            
+            SizedBox(height: 25),
             Center(
               child: IconButton(
-              icon: Icon(Icons.add_a_photo, size: 70, color: Colors.blue),
-              onPressed: () {
-                // Lógica para adicionar foto
-              },
+                icon: Icon(Icons.add_a_photo, size: 70, color: Colors.blue),
+                onPressed: _pickImage, // Chama o método para capturar a imagem
+              ),
             ),
-            ),
-            
-            SizedBox(height: 25),
+            SizedBox(height: 20),
+            if (_image != null) // Verifica se há uma imagem capturada
+              Center(
+                child: Image.file(
+                  _image!,
+                  width: 200,
+                  height: 200,
+                ),
+              ),
             CheckboxListTile(
               title: Text('Turva'),
               value: isTurvaSelected,
